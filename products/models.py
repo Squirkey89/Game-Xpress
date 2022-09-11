31f1a5c74e23ab55152518d2fb477391920f3b04
Squirkey89 <squirke89@gmail.com>
2022-09-11 15:33:38 +0000

    added category filtering, product details page and search functionality

--- products/models.py
+++ products/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
 
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
- 
+
     def __str__(self):
         return self.name
 
@@ -19,8 +19,8 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    description = models.CharField(max_length=254, null=True, blank=True)
-    genre = models.CharField(max_length=254, null=True, default=True)
+    description = models.TextField()
+    genre = models.CharField(max_length=254, blank=True, default=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
