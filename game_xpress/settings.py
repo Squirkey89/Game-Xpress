d346b3dd2dd270c6feb3e242eddc41009b3de281
Squirkey89 <squirke89@gmail.com>
2022-09-19 13:47:44 +0000

    Changed to secret key back to work on local environment

--- game_xpress/settings.py
+++ game_xpress/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'django-insecure-ofr+xp8&*ac%m7smn)7di6z81yy10mt*_f!pbuaghopwt7+_l2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
