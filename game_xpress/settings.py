0e15196b048388b556d9f9e1ef78515492519416
Squirkey89 <squirke89@gmail.com>
2022-09-19 10:36:06 +0000

    Deploying to heroku part 2

--- game_xpress/settings.py
+++ game_xpress/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-ofr+xp8&*ac%m7smn)7di6z81yy10mt*_f!pbuaghopwt7+_l2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gamexpress.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,13 +119,17 @@ WSGI_APPLICATION = 'game_xpress.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
