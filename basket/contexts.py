78f91482c6e757804b74516a88c70ed35d083df9
Squirkey89 <squirke89@gmail.com>
2022-09-22 12:11:06 +0000

    Added coupon logic to context.py and coupon code to basket.html

--- basket/contexts.py
+++ basket/contexts.py
@@ -2,20 +2,36 @@ from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
+from .models import Coupon
+
 
 
 def basket_contents(request):
+    coupon_id = request.session.get('discount_id', int())
 
     basket_items = []
     total = 0
-    product_count = 0
+    savings = 0
+    coupon_amount = 0
     basket_total = 0
+    product_count = 0
     basket = request.session.get('basket', {})
+
+    try:
+        coupon = Coupon.objects.get(id=coupon_id)
+
+    except Coupon.DoesNotExist:
+        coupon = None
 
     for item_id, quantity in basket.items():
         product = get_object_or_404(Product, pk=item_id)
         basket_total += quantity * product.price
-        total = basket_total
+        if coupon is not None:
+            coupon_amount = coupon.amount
+            savings = basket_total*(coupon_amount/Decimal('100'))
+            total = basket_total - savings
+        else:
+            total = basket_total
             
         product_count += quantity
         basket_items.append({
@@ -41,7 +57,9 @@ def basket_contents(request):
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
-        
+        'coupon': coupon,
+        'coupon_amount': coupon_amount,
+        'savings': savings,
     }  
 
 
