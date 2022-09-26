f10b72c608968124e09854e2db9cd35ad631f8dc
Squirkey89 <squirke89@gmail.com>
2022-09-26 09:35:51 +0000

    Added review model and admin, Added back to top button on small devices

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Review
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
@@ -28,5 +28,17 @@ class CategoryAdmin(admin.ModelAdmin):
     )
 
 
+class ReviewsAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('added_by', )
+
+    list_display = (
+        'review',
+        'user',
+        'rating',
+    )
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Review, ReviewsAdmin)
--- products/models.py
+++ products/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Category(models.Model):
@@ -16,15 +18,61 @@ class Category(models.Model):
         return self.friendly_name
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+    category = models.ForeignKey(
+        'Category',null=True, 
+        blank=True, on_delete=models.SET_NULL
+        )
+    sku = models.CharField(
+        max_length=254, null=True, blank=True
+        )
     name = models.CharField(max_length=254)
     description = models.TextField()
-    genre = models.CharField(max_length=254, blank=True, default=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    genre = models.CharField(
+        max_length=254, blank=True, default=True
+        )
+    price = models.DecimalField(
+        max_digits=6, decimal_places=2
+        )
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, 
+        null=True, blank=True)
+    image_url = models.URLField(
+        max_length=1024, null=True, blank=True
+        )
+    image = models.ImageField(
+        null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    title = models.CharField(
+        verbose_name=('Review Title'),
+        max_length=25,
+        null=False,
+        blank=False
+        )
+    User = models.ForeignKey(User, 
+        on_delete=models.CASCADE
+        )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+        )
+    created_on = models.DateTimeField(
+        auto_now_add=True
+        )
+    review = models.TextField(null=True, 
+        blank=False
+        )
+    rating = models.IntegerField(default=0, 
+                    validators=[
+                    MinValueValidator(1),
+                    MaxValueValidator(5)])
+   
+    
+
+    def __str__(self):
+        return self.review
+
