2319034b18b45dd75554308a8402bbe11dc116ed
Squirkey89 <squirke89@gmail.com>
2022-09-28 13:46:19 +0000

    Fixed issues to send confirmation email

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -73,8 +73,8 @@ class StripeWH_Handler:
                 profile.default_country = shipping_details.address.country
                 profile.default_postcode = shipping_details.address.postal_code
                 profile.default_town_or_city = shipping_details.address.city
-                profile.default_street_address1 = shipping_details.address.lne1
-                profile.default_street_address2 = shipping_details.address.lne2
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
                 profile.default_county = shipping_details.address.state
                 profile.save()
 
@@ -144,7 +144,7 @@ class StripeWH_Handler:
             content=(f'Webhook received: {event["type"]} | SUCCESS: '
                      'Created order in webhook'),
             status=200)
-
+            
 
     def handle_payment_intent_payment_failed(self, event):
         """
